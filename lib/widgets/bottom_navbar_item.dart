62027e383cbc9422ad55b76a729563fdf30a2ac2
Farizal hendra Putra Kusuma <43551882+Farizal97@users.noreply.github.com>
2021-08-26 08:55:08 +0700

    fix spasi buttom

--- lib/widgets/bottom_navbar_item.dart
+++ lib/widgets/bottom_navbar_item.dart
@@ -1,3 +1,4 @@
+import 'package:cari_kos/theme.dart';
 import 'package:flutter/material.dart';
 
 class BottomNavbarItem extends StatelessWidget {
@@ -5,10 +6,22 @@ class BottomNavbarItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        Spacer(),
         Image.asset(
           'assets/icon_home.png',
           width: 26,
         ),
+        Spacer(),
+        Container(
+          width: 30,
+          height: 4,
+          decoration: BoxDecoration(
+            color: purpleColor,
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(1000),
+            )
+          ),
+        )
       ],
     );
   }
